95cfba4ba74cba9484449b642a889e1aa994d104
scarlet0star <scarlet0star@gmail.com>
2023-03-14 20:30:32 +0900

    0314

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 from pymongo import MongoClient
-app = Flask(__name__)
+from flask_cors import CORS
 
+app = Flask(__name__,static_folder="templates/assets")
+CORS(app)
 
-@app.route('/details/<usernum>',method=["GET"])
+client = MongoClient('mongodb+srv://kimes:1234abcd@cluster0.d5w5umq.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+userComment = db.comment
+
+@app.route('/')
+def home():
+    comments = list(userComment.find({},{'_id':False}))
+    return render_template('index.html',comments=comments)
+
+@app.route('/details/<usernum>',methods=["GET"])
 def detail(usernum):
     pass
 
-@app.route('/comment',method=["POST","GET"])
+@app.route('/comment', methods=["POST"])
 def comment():
     if request.method == "POST":
-        pass
-    elif request.method == "GET":
-        pass
+        form = request.form
+        doc = {
+            'name' : form.get('writer'),
+            'title' : form.get('title'),
+            'star' : int(form.get('star')),
+            'comment' : form.get('comment')
+        }
+        userComment.insert_one(doc)
+        print(doc)
+        return jsonify({"message": "Comment added successfully."})
+      
+if __name__ == '__main__':
+    app.run(debug=True)
