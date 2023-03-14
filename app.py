4690c5c8697113c9a3caa2c2aee02ccedea3df03
KimEunSu <misili17114@gmail.com>
2023-03-14 19:23:31 +0900

    수정페이지 추가

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://kimes:1234abcd@cluster0.d5w5umq.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return 'This is Home!'
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
